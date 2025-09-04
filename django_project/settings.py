35fe902b3745fd007521b2233938a4da6fe8a441
Kristina <krasilnikova13570@gmail.com>
2025-09-04 21:43:33 +0300

    email

--- django_project/settings.py
+++ django_project/settings.py
@@ -158,7 +158,7 @@ STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = "accounts.CustomUser"
-LOGIN_REDIRECT_URL = 'home'
+LOGIN_REDIRECT_URL = 'home' 
 ACCOUNT_LOGOUT_REDIRECT = "home"
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
@@ -170,3 +170,5 @@ ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_AUTHENTICATION_METHOD = "email"
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_UNIQUE_EMAIL = True
+DEFAULT_FROM_EMAIL = "admin@djangobookstore.com"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
