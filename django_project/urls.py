f640211be4d05a0ba8ee9860f66122811e3dc3a1
Kristina <krasilnikova13570@gmail.com>
2025-09-16 11:43:39 +0300

    file/image uploads

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -10,7 +12,7 @@ urlpatterns = [
     path('', include('pages.urls')),
     path('accounts/', include('accounts.urls')),
     path("books/", include("books.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 
